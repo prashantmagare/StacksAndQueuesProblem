14e994bc3607abfd415b4629e2e80671db1e3296
prashantmagare <prashantmagare3010@gmail.com>
2022-05-29 22:25:35 +0530

    "Added - UC1_Stacks"

--- StacksAndQueues/StacksAndQueues/Program.cs
+++ StacksAndQueues/StacksAndQueues/Program.cs
@@ -7,6 +7,16 @@ namespace Stacks_And_Queues
         static void Main(string[] args)
         {
             Console.WriteLine("StacksAndQueues");
+                      
+                Console.WriteLine("Pushing values to Stack");
+                LinkedListStack linkedListStack = new LinkedListStack();
+                linkedListStack.push(70);
+                linkedListStack.push(30);
+                linkedListStack.push(56);
+                linkedListStack.Display();
+
+        
         }
     }
 }
+    
