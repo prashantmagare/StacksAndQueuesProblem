a1a01e5c08d114481009a58ff6f2fbb3040e06a7
prashantmagare <prashantmagare3010@gmail.com>
2022-05-29 22:17:51 +0530

    "Added : StacksAndQueuesProgram"

---
+++ StacksAndQueues/StacksAndQueues/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Stacks_And_Queues
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("StacksAndQueues");
+        }
+    }
+}
