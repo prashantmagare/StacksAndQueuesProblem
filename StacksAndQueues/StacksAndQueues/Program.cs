0d9c8ad3cb1a20dd53447269cc6f1f3b222407bb
prashantmagare <prashantmagare3010@gmail.com>
2022-05-29 22:33:14 +0530

    "Added - UC2_Stacks"

--- StacksAndQueues/StacksAndQueues/Program.cs
+++ StacksAndQueues/StacksAndQueues/Program.cs
@@ -6,17 +6,18 @@ namespace Stacks_And_Queues
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("StacksAndQueues");
-                      
-                Console.WriteLine("Pushing values to Stack");
-                LinkedListStack linkedListStack = new LinkedListStack();
-                linkedListStack.push(70);
-                linkedListStack.push(30);
-                linkedListStack.push(56);
-                linkedListStack.Display();
+            Console.WriteLine("Pushing values to Stack");
+            LinkedListStack linkedListStack = new LinkedListStack();
+            linkedListStack.push(70);
+            linkedListStack.push(30);
+            linkedListStack.push(56);
+            linkedListStack.Display();
+            Console.WriteLine("Using Peek and Pop Till Stack is Empty");
+            linkedListStack.Peek();
+            linkedListStack.Pop();
+            linkedListStack.IsEmpty();
+            linkedListStack.Display();
 
-        
         }
     }
 }
-    
