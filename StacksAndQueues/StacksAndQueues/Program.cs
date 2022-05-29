f414d8b4b3d7832d41d24ab64e4d4d105b46f01f
prashantmagare <prashantmagare3010@gmail.com>
2022-05-29 22:38:28 +0530

    "Added - UC3_Queues"

--- StacksAndQueues/StacksAndQueues/Program.cs
+++ StacksAndQueues/StacksAndQueues/Program.cs
@@ -6,17 +6,26 @@ namespace Stacks_And_Queues
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Pushing values to Stack");
-            LinkedListStack linkedListStack = new LinkedListStack();
-            linkedListStack.push(70);
-            linkedListStack.push(30);
-            linkedListStack.push(56);
-            linkedListStack.Display();
-            Console.WriteLine("Using Peek and Pop Till Stack is Empty");
-            linkedListStack.Peek();
-            linkedListStack.Pop();
-            linkedListStack.IsEmpty();
-            linkedListStack.Display();
+            Console.WriteLine("StacksAndQueues");
+
+            //Console.WriteLine("Pushing values to Stack");
+            //LinkedListStack linkedListStack = new LinkedListStack();
+            //linkedListStack.push(70);
+            //linkedListStack.push(30);
+            //linkedListStack.push(56);
+            //linkedListStack.Display();
+            //Console.WriteLine("Using Peek and Pop Till Stack is Empty");
+            //linkedListStack.Peek();
+            //linkedListStack.Pop();
+            //linkedListStack.IsEmpty();
+            //linkedListStack.Display();
+            LinkedList_Queues linkedListQueues = new LinkedList_Queues();
+            linkedListQueues.Enqueue(56);
+            linkedListQueues.Enqueue(30);
+            linkedListQueues.Enqueue(70);
+            linkedListQueues.Display();
+
+
 
         }
     }
